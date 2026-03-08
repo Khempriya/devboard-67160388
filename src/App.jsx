97085d7616a5c2a93e9691144169065c3de85798
Khempriya Booncharoen <67160388@go.buu.ac.th>
2026-03-09 00:42:57 +0700

    feat: update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import PostList from "./components/PostList";
 import UserCard from "./components/UserCard";
-
-const POSTS = [
+import AddPostForm from "./components/AddPostForm";
+const INITIAL_POST = [
   {
     id: 1,
     title: "React คืออะไร?",
@@ -32,9 +33,28 @@ const USERS = [
 ];
 
 function App() {
+  const [posts, setPosts] = useState(INITIAL_POST);
+  const [favorites, setFavorites] = useState([]); /* เก็บ id ที่ถูกใจ */
+  /* Toggle ถูกใจ/ยกเลิก */
+  function handleToggleFavorite(postId) {
+    setFavorites(
+      (prev) =>
+        prev.includes(postId)
+          ? prev.filter((id) => id !== postId) /* ลบออก */
+          : [...prev, postId] /* เพิ่มเข้า */,
+    );
+  }
+  /* เพิ่มโพสต์ใหม่ */
+  function handleAddPost({ title, body }) {
+    const newPost = {
+      id: Date.now() /* ใช้ timestamp เป็น id ชั่วคราว */,
+      title,
+      body,
+    };
+    setPosts((prev) => [newPost, ...prev]); /* เพิ่มไว้ด้านบน */
+  }
   return (
     <div>
-      <Navbar />
       <div
         style={{
           maxWidth: "900px",
@@ -47,7 +67,12 @@ function App() {
       >
         {/* คอลัมน์ซ้าย: โพสต์ */}
         <div>
-          <PostList posts={POSTS} />
+          <AddPostForm onAddPost={handleAddPost} />
+          <PostList
+            posts={posts}
+            favorites={favorites}
+            onToggleFavorite={handleToggleFavorite}
+          />
         </div>
 
         {/* คอลัมน์ขวา: สมาชิก */}
